0e342b5baca026ab20536d6aa6e2fc2b59a21d04
Faïzou Aremou <faizouaremou@gmail.com>
2023-07-11 14:15:31 +0200

    route guard

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,15 @@
-import { Router } from 'express';
+import { NextFunction, Router } from 'express';
 import { RequestWithBody } from '../models/requestWithBody';
+import { Request, Response } from 'express';
+
+function requireAuthMiddleware(req: Request, res: Response, next: NextFunction): void {
+  if (req?.session?.loggedIn) {
+    next();
+  } else {
+    res.status(403);
+    res.send('Not permitted');
+  }
+}
 
 export const router = Router();
 
@@ -21,9 +31,37 @@ router.get('/login', (req, res) => {
 
 router.post('/login', (req: RequestWithBody, res) => {
   const { email, password } = req.body;
-  if (email) {
-    res.send(email.toUpperCase());
+  if (email === 'hi@hi.com' && password === 'password') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
   } else {
-    res.send('You must provide email');
+    res.send('Invalid email or password');
   }
 });
+
+router.get('/', (req, res) => {
+  if (req?.session?.loggedIn) {
+    res.send(`
+    <div>
+    <div> You are logged in</div>
+    <a href="/logout">Logout</a>
+    </div>
+`);
+  } else {
+    res.send(`
+    <div>
+    <div> You are not logged in</div>
+    <a href="/login">Login</a>
+    </div>
+    `);
+  }
+});
+
+router.get('/logout', (req: RequestWithBody, res) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuthMiddleware, (req, res)=>{
+res.send('Welcome to the protected route logged User')
+})
